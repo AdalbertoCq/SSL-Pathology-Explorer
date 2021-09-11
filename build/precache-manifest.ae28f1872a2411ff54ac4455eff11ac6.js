1dd3707fb74d909585c1d738efdfade318ba496a
AdalbertoCq <aclaudioquiros@gmail.com>
2021-09-11 11:05:34 +0100

    First draft

---
+++ build/precache-manifest.ae28f1872a2411ff54ac4455eff11ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54143372e35137a5b013",
+    "url": "/ssl-umap-explorer/static/css/main.a865a61c.chunk.css"
+  },
+  {
+    "revision": "54143372e35137a5b013",
+    "url": "/ssl-umap-explorer/static/js/main.54143372.chunk.js"
+  },
+  {
+    "revision": "fb74cf2e345109eb41a6",
+    "url": "/ssl-umap-explorer/static/js/1.fb74cf2e.chunk.js"
+  },
+  {
+    "revision": "05e2a37e94bde7db1276",
+    "url": "/ssl-umap-explorer/static/js/runtime~main.05e2a37e.js"
+  },
+  {
+    "revision": "0dc6eda33ac53a304fa086dc5c474672",
+    "url": "/ssl-umap-explorer/index.html"
+  }
+];
