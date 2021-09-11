34a891a83e18e742998b3de258cba2a914442da5
AdalbertoCq <aclaudioquiros@gmail.com>
2021-09-11 11:41:37 +0100

    Updates

---
+++ precache-manifest.8b121b42985d2733734db52354e601de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e30116de0eadad1be6e",
+    "url": "/SSL-Pathology-Explorer/static/css/main.a865a61c.chunk.css"
+  },
+  {
+    "revision": "7e30116de0eadad1be6e",
+    "url": "/SSL-Pathology-Explorer/static/js/main.7e30116d.chunk.js"
+  },
+  {
+    "revision": "fb74cf2e345109eb41a6",
+    "url": "/SSL-Pathology-Explorer/static/js/1.fb74cf2e.chunk.js"
+  },
+  {
+    "revision": "76a1f4c6072ede9acafe",
+    "url": "/SSL-Pathology-Explorer/static/js/runtime~main.76a1f4c6.js"
+  },
+  {
+    "revision": "aa8021217b832b9cddecf9909e28cca6",
+    "url": "/SSL-Pathology-Explorer/index.html"
+  }
+];
